d7dce07a58e54970673a35d5c5b95bde846c42a3
Pavan Subhash Tirumalasetti <pavansubhash_t@infosys.com>
2020-03-21 22:19:01 +0530

    Changed Few files

--- app.py
+++ app.py
@@ -14,15 +14,16 @@ from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 from PIL import Image
 from io import BytesIO
+from camera import VideoCamera
+
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-
+    
 app = Flask(__name__)
 
 def findPeople(features_arr, positions, thres = 0.6, percent_thres = 70):
 
-    f = open('./facerec_128D.txt','r')
-    data_set = json.loads(f.read())
+
     returnRes = []
     for (i,features_128D) in enumerate(features_arr):
         result = "Unknown"
@@ -41,7 +42,6 @@ def findPeople(features_arr, positions, thres = 0.6, percent_thres = 70):
     return returnRes
 
 def detect_faces(image):
-    img = Image.open(BytesIO(image))
     rects, landmarks = face_detect.detect_face(np.array(img),80);#min face size is set to 80x80
     aligns = []
     positions = []
@@ -68,21 +68,44 @@ def detect_faces(image):
     except:
         return [], []
 
-@app.route("/")
-def main():
-    return render_template("index.html")
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        rects, landmarks = face_detect.detect_face(frame,80);#min face size is set to 80x80
+        aligns = []
+        positions = []
+
+        for (i, rect) in enumerate(rects):
+            aligned_face, face_pos = aligner.align(160,frame,landmarks[:,i])
+            if len(aligned_face) == 160 and len(aligned_face[0]) == 160:
+                aligns.append(aligned_face)
+                positions.append(face_pos)
+            else: 
+                print("Align face failed") #log        
+        if(len(aligns) > 0):
+            features_arr = extract_feature.get_features(aligns)
+            recog_data = findPeople(features_arr,positions)
+            for (i,rect) in enumerate(rects):
+                print (rect)
+                cv2.rectangle(frame,(rect[0],rect[1]),(rect[2],rect[3]),(255,0,0)) #draw bounding box for the face
+                cv2.putText(frame,recog_data[i][0]+" - "+str(recog_data[i][1])+"%",(rect[0],rect[1]),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),1,cv2.LINE_AA)
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-@app.route("/prediction", methods=["POST"])
-def prediction():
-    """
-    curl -X POST -v -H "Content-Type: image/png" --data-binary @abba.png http://127.0.0.1:9099/prediction -o foo.jpg
-    """
-    if request.method == "POST":
-        image = request.data
-        face_coordinates, recog_data = detect_faces(image)
-        
-        return jsonify(faces=face_coordinates, people=recog_data)
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+@app.route('/recognize', methods=['GET'])
+def recognize():
+    if request.method == 'GET':
+        camera_id = int(request.values.get('camid'))
+        print (camera_id)
+        return Response(gen(VideoCamera(camera_id)),mimetype='multipart/x-mixed-replace; boundary=frame') 
+    
 
 if __name__ == '__main__':
     FRGraph = FaceRecGraph()
@@ -90,6 +113,8 @@ if __name__ == '__main__':
     aligner = AlignCustom()
     extract_feature = FaceFeature(FRGraph)
     face_detect = MTCNNDetect(MTCNNGraph, scale_factor=2); #scale_factor, rescales image for faster detection
+    f = open('./facerec_128D.txt','r')
+    data_set = json.loads(f.read())
     http_server = WSGIServer(('0.0.0.0',5000), app)
     http_server.serve_forever()        
 
